819ce8c7d63203224397c30351ca479b9c9ee711
Adam Bray <adam.bray@yale.edu>
2016-02-10 06:32:05 -0500

    adds helper method to convert results to objects for improved reusability

--- demos/bar_charts/highcharts_demo.js
+++ demos/bar_charts/highcharts_demo.js
@@ -1,17 +1,37 @@
 $(document).ready(function(){
   // This will get the first returned node in the jQuery collection.
   $.getJSON("http://api.census.gov/data/timeseries/asm/state?get=NAICS_TTL,EMP,PAYANN,GEO_TTL&for=state:*&YEAR=2014&NAICS=31-33&key=81cdc733d3ac0f3496a88eebbed0a31478c403c6")
+  .then(convertResultsToObjects)
   .then(extractAvgSalary)
   .then(graphAvgSalary);
 });
+
+function convertResultsToObjects(results) {
+  var objectArray = [];
+  var headers = results[0];
+  var numCols = headers.length;
+  var numRows = results.length;
+
+  for(var rowNum = 1; rowNum < numRows; rowNum++){
+    var currentRow = results[rowNum];
+    var newObj = {};
+    for(var colNum = 0; colNum < numCols; colNum++) {
+      var header = headers[colNum];
+      newObj[header] = currentRow[colNum];
+    }
+    objectArray.push(newObj);
+  }
+
+  return objectArray;
+}
 
 function extractAvgSalary(results) {
   var dataPoints = [];
   for(var i = 1; i < results.length; i++){
     var currentResult = results[i];
-    var state = currentResult[3];
-    var numEmployees = parseInt(currentResult[1]);
-    var annPayroll = parseInt(currentResult[2]) * 1000;
+    var state = currentResult.STATE;
+    var numEmployees = parseInt(currentResult.EMP);
+    var annPayroll = parseInt(currentResult.PAYANN) * 1000;
 
     if (numEmployees !== 0 && annPayroll !== 0) {
       var avgSalary = Math.round(annPayroll / numEmployees);
