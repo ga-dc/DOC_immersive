ee33b3a01b0e83e467d330d5ef9323c3022f0bc9
Adam Bray <adam.bray@yale.edu>
2016-02-22 22:13:31 -0500

    refactored line chart using highcharts

--- demos/html_page/js/employment_by_category.js
+++ demos/html_page/js/employment_by_category.js
@@ -5,8 +5,25 @@ function graphNAICSCode(naicsCode) {
     $.getJSON("http://api.census.gov/data/timeseries/asm/industry?get=NAICS_TTL,EMP,GEO_TTL&for=us:*&YEAR=2005,2006,2007,2008,2009,2010,2011,2012,2013,2014&NAICS=" + naicsCode + "&key=81cdc733d3ac0f3496a88eebbed0a31478c403c6")
     .then(convertResultsToObjects)
     .then(function(results) {
-      var data = _.map(results, function(r) { return [r.YEAR, r.EMP]; });
-      graph(data, $("#employment-by-category").get(0));
+      $("#employment-by-category").highcharts({
+        chart: { type: 'line'},
+        title: { text: 'Employment by Code'},
+        xAxis: {
+          categories: _.map(results, function(d) {return parseInt(d.YEAR); })
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: "Average Salary"
+          }
+        },
+        series: [
+          {
+            showInLegend: false,
+            data: _.map(results, function(d) {return parseInt(d.EMP); })
+          }
+        ]
+      });
     });
 
 }
